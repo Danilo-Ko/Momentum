906fdef9c338ad7b06260710f4cb5fcbc074cb45
Danilo-Ko <102959984+Danilo-Ko@users.noreply.github.com>
2022-06-14 21:29:35 +0200

    요일 표시, 각종 사이트 연결

--- js/clock.js
+++ js/clock.js
@@ -2,26 +2,29 @@ const getDate = document.querySelector("#date");
 
 let year = new Date ();
 let month = new Date ();
-let day = new Date ();
+let date = new Date ();
+let day = new Date();
+const WEEKDAY = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일','토요일'];
+let week = WEEKDAY[day.getDay()];
 
-getDate.innerText = `${year.getFullYear()}년 ${month.getMonth()+1}월 ${day.getDay()}일`;
+
+getDate.innerText = `${year.getFullYear()}년 ${month.getMonth()+1}월 ${date.getDate()}일 \r\n${week}`;
 
 
 const clock = document.querySelector("h2#clock");
 
 function getClock () {
  
-    const data = new Date();
-    const hours = String(data.getHours()).padStart(2,"0");
-    const minutes = String(data.getMinutes()).padStart(2,"0");
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
     // const seconds = String(data.getSeconds()).padStart(2,"0");
 
     clock.innerText = `${hours}:${minutes}`;
 }
 
-/*:${seconds}*/
-
 getClock();
-setInterval(getClock,  1000);
+setInterval(getClock, 1000);
 
 
+    
